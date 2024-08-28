2c60557af7d538e19b08ca84ccba1ddbf52fa228
bek <shoyatbekov03032003@gmail.com>
2024-08-28 12:49:52 +0500

    test optimization 1.0.1: login error

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -7,6 +7,17 @@ export function getLogin(req, res, next) {
 
 export function login(req, res, next) {
   try {
+    console.log(
+      "username: %s and password: %s from req.body\n",
+      req.body?.username,
+      req.body?.password
+    );
+
+    console.log(
+      "username: %s && password %s",
+      config.adminLogin,
+      config.adminPassword
+    );
     if (
       req.body.username !== config.adminLogin ||
       req.body.password !== config.adminPassword
--- src/utils/db/connectDB.js
+++ src/utils/db/connectDB.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 import config from "../../config/index.js";
 
 const connectDb = async () => {
-    try {
-        await mongoose
-            .connect(config.mongoUri)
-        console.log("Connected to MongoDB");
-    } catch (err) {
-        console.log("Error connecting to MongoDB Atlas", err);
-    }
+  try {
+    await mongoose.connect(config.mongoUri);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("Error connecting to MongoDB Atlas", err);
+  }
 };
 
 export default connectDb;
